2902e898a8522ffc795e8c6e4462c5b02c73fd63
anton.akushev <overdrowe@gmail.com>
2023-08-12 23:18:17 +0500

    first commit

---
+++ geekbrains_ui/geekbrains_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  geekbrains_uiApp.swift
+//  geekbrains_ui
+//
+//  Created by Антон on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct geekbrains_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
