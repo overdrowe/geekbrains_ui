d7d170c05c3ebd7c3c239e818aa2d1d066e81011
anton.akushev <overdrowe@gmail.com>
2023-08-13 00:05:26 +0500

    login view

--- geekbrains_ui/ContentView.swift
+++ geekbrains_ui/ContentView.swift
@@ -1,26 +1,52 @@
-//
-//  ContentView.swift
-//  geekbrains_ui
-//
-//  Created by Антон on 12.08.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State var loginValue = ""
+	@State var passwordValue = ""
+	
+	var body: some View {
+		Color.red
+			.opacity(0.3)
+			.ignoresSafeArea()
+			.overlay(
+				VStack(alignment: .center) {
+					Text("Welcome!")
+						.bold()
+						.font(.title)
+						.foregroundColor(.accentColor)
+					VStack {
+						TextField("login", text: $loginValue)
+						SecureField("password", text: $passwordValue)
+					}
+					.frame(width: 256)
+					.textFieldStyle(.roundedBorder)
+					.opacity(0.7)
+					HStack {
+						Spacer()
+						Button("Login", action: loginClicked)
+					}
+					.frame(width: 256)
+				}
+					.frame(width: 300, height: 200)
+					.padding()
+					.background(.white.opacity(0.2))
+					.cornerRadius(12)
+					
+			)
+		
+	}
+}
+
+
+// MARK: - login action
+extension ContentView {
+	func loginClicked() {
+		print(loginValue + " " + passwordValue)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
